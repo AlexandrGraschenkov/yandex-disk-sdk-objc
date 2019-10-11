daefd04cf0ab21fba97855a5fce0173bdd87546e
Sebastian Husche <sebastian@scanbot.io>
2019-10-11 13:57:58 +0200

    Build framework for iOS

---
+++ YandexSDK/YandexSDK.h
@@ -0,0 +1,22 @@
+//
+//  YandexSDK.h
+//  YandexSDK
+//
+//  Created by Sebastian Husche on 11.10.19.
+//  Copyright © 2019 Yandex. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YandexSDK.
+FOUNDATION_EXPORT double YandexSDKVersionNumber;
+
+//! Project version string for YandexSDK.
+FOUNDATION_EXPORT const unsigned char YandexSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YandexSDK/PublicHeader.h>
+
+#import <YandexSDK/YDSession.h>
+#import <YandexSDK/YDConstants.h>
+#import <YandexSDK/YOAuth2Delegate.h>
+#import <YandexSDK/YOAuth2ViewController.h>
